500c209347cce49bb9f1923597b8dcfac3af81a0
bvenceslas <bvenceslas@gmail.com>
2021-03-07 20:28:57 +0200

    Add the about model

---
+++ models/about.model.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const AboutSchema = mongoose.Schema({
+    text: {
+        type: String, 
+        required: true
+    }
+});
+
+module.exports = mongoose.model('impacts', AboutSchema);
